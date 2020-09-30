0dbebe5e31f00efb7181044917157753f5069fd0
kspace-trk <keigonakao.0825@gmail.com>
2020-09-30 12:19:49 +0900

    [done]課題

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset=“UTF-8” />
-    <title>ページタイトル</title>
-</head>
-
-<body>
-    <?php
-    echo "<p>こんにちはPHP</p>"
-    ?>
-</body>
-
-</html>
+<?php 
+ echo '<!DOCTYPE html>
+ <html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>ページタイトル</title>
+    </head>
+    <body>
+        <p> Hello PHP world! </p>
+    </body>
+</html>'
+?>
