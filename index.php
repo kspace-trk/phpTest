2379f1d20cdbfb578b7050a636a3f8825e46bc80
kspace-trk <keigonakao.0825@gmail.com>
2020-10-05 11:16:09 +0900

    [done]課題3-2

--- index.php
+++ index.php
@@ -1,12 +1,59 @@
-<?php 
- echo '<!DOCTYPE html>
- <html>
+<!DOCTYPE html>
+<html>
     <head>
         <meta charset="UTF-8" />
         <title>ページタイトル</title>
     </head>
     <body>
-        <p> Hello PHP world! </p>
+        <table>
+                <!---------一番上の行-------->
+                <tr class="color-cell">
+                <td>x</td>
+                <?php
+                    for($i = 1; $i <10; $i++){
+                        $x = "<td>$i</td>";
+                        echo $x;
+                    }
+                ?>
+                </tr>
+                <!---------一番上の行-------->
+                    <?php
+                        $trStart = "<tr>";
+                        $trEnd = "</tr>";
+                        $i = 1;
+                        
+                        for($n = 1; $n < 10; $n++){
+                            echo $trStart;
+                            $x = "<td class='color-cell'>$i</td>";
+                            $i++;
+                            echo $x;
+                            for($m = 1; $m < 10; $m++){
+                            $x = $n * $m;
+                            echo "<td>$x</td>";
+                            }
+                            echo $trEnd;
+                        }
+                        
+
+                    ?>
+        </table>
     </body>
-</html>'
-?>
+</html>
+<style>
+body{
+    display: grid;
+    place-items: center;
+}
+table{
+    border: 1px solid #707070;
+}
+td{
+     border: 1px solid #707070;
+     padding: 5px;
+}
+.color-cell{
+    background-color: #707070;
+    color: #ffffff;
+}
+</style>
+    
