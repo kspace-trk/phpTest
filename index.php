6f97a50c0c6cac6c83a1bf43b150b7194ee00705
kspace-trk <keigonakao.0825@gmail.com>
2020-10-05 11:17:43 +0900

    3-1

--- index.php
+++ index.php
@@ -1,59 +1,14 @@
 <!DOCTYPE html>
-<html>
+ <html>
     <head>
         <meta charset="UTF-8" />
         <title>ページタイトル</title>
     </head>
     <body>
-        <table>
-                <!---------一番上の行-------->
-                <tr class="color-cell">
-                <td>x</td>
-                <?php
-                    for($i = 1; $i <10; $i++){
-                        $x = "<td>$i</td>";
-                        echo $x;
-                    }
-                ?>
-                </tr>
-                <!---------一番上の行-------->
-                    <?php
-                        $trStart = "<tr>";
-                        $trEnd = "</tr>";
-                        $i = 1;
-                        
-                        for($n = 1; $n < 10; $n++){
-                            echo $trStart;
-                            $x = "<td class='color-cell'>$i</td>";
-                            $i++;
-                            echo $x;
-                            for($m = 1; $m < 10; $m++){
-                            $x = $n * $m;
-                            echo "<td>$x</td>";
-                            }
-                            echo $trEnd;
-                        }
-                        
-
-                    ?>
-        </table>
+        <?php 
+            for($i = 1; $i <= 500 ; $i++){
+                echo '<p>'.$i.'行目　ダミーテキスト</p>';
+            }
+        ?>
     </body>
 </html>
-<style>
-body{
-    display: grid;
-    place-items: center;
-}
-table{
-    border: 1px solid #707070;
-}
-td{
-     border: 1px solid #707070;
-     padding: 5px;
-}
-.color-cell{
-    background-color: #707070;
-    color: #ffffff;
-}
-</style>
-    
