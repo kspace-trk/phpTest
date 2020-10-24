d0de1246cd42c53737d3faf21cf0845497ceaa4e
kspace-trk <keigonakao.0825@gmail.com>
2020-10-25 04:16:18 +0900

    done

--- index.php
+++ index.php
@@ -9,24 +9,44 @@
     <body>
     <?php
         $dbname = 'testdb2';
-        $tablename = 'table1';
+        $tablename = 'money_management_list';
 
         $link = mysqli_connect('127.0.0.1','root','dbpass',$dbname);
         if(! $link){ exit("Connect error!"); }
 
         $result = mysqli_query($link,"SELECT * FROM $tablename");
         if(!$result){ exit("Select error on table ($tablename)!"); } 
-
-        while($row = mysqli_fetch_row($result))
-        {
-            echo "<hr>";
-            foreach($row as $key => $value)
+        echo "<table>";
+            echo "<tr>";
+                echo "<td>";
+                    echo "id";
+                echo "</td>";
+                echo "<td>";
+                    echo "日付";
+                echo "</td>";
+                echo "<td>";
+                    echo "収入";
+                echo "</td>";
+                echo "<td>";
+                    echo "支出";
+                echo "</td>";
+                echo "<td>";
+                    echo "メモ";
+                echo "</td>";
+            echo "</tr>";
+            while($row = mysqli_fetch_row($result))
             {
-                echo htmlspecialchars($key) . "  : ";
-                echo htmlspecialchars($value) . "<br>";
+                
+                echo "<tr>";
+                foreach($row as $key => $value)
+                {
+                    echo "<td>";
+                        echo htmlspecialchars($value);
+                    echo "</td>";
+                }
             }
-        }
-        echo "<hr>";
+            echo "</tr>";
+        echo "</table>";
 
         mysqli_free_result($result);
 
