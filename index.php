65e2431bc695151a208dcbe009087462d7ef778c
kspace-trk <keigonakao.0825@gmail.com>
2020-10-25 02:23:46 +0900

    動かない進捗

--- index.php
+++ index.php
@@ -2,41 +2,46 @@
 <html>
     <head>
         <meta charset="UTF-8" />
-        <title>なんちゃって掲示板システム</title>
+        <!-- vim: set sts sw=4 expandtab : -->
+        <title>ページタイトル</title>
     </head>
     <body>
-        <div class="container">
-        <p>なんちゃって掲示板システム</p>
-            <button onclick="location.href='articlePost.php'">新規投稿</button>
-        </div>
+<?php
+    $hostname = '127.0.0.1';
+    $username = 'root';
+    $password = 'dbpass';
+    $dbname = 'testdb2';
+    $tablename = 'money_management_list';
+
+    $link = mysqli_connect($hostname,$username,$password);
+    if(! $link){
+        exit("Connect error!");
+    }
+
+    $result = mysqli_query($link,"CREATE DATABASE $dbname CHARACTER SET utf8");
+    if(!$result) {
+        echo "Create database $dbname failed!\n";
+    }
+
+    $result = mysqli_query($link,"USE $dbname");
+    if(!$result) {
+        exit("USE failed!");
+    }
+
+    $result = mysqli_query($link,"CREATE TABLE $tablename (id INT NOT NULL AUTO_INCREMENT, today DATE, inprice INT, outprice INT, memo VARCHAR(30) BINARY, PRIMARY KEY(id)) CHARACETR SET utf8");
+    if(!$result) {
+        exit("Create table $tablename failed!\n");
+    }
+
+    $result = mysqli_query($link,"INSERT INTO $tablename SET id=0, today='2020-10-25', inPrice=20000, outPrice=0, memo='work'");
+    if(! $result){
+        exit("INSERT error!");
+    }
+
+    echo "Create db and table and update succeeded!\n";
+
+    mysqli_close($link);
+?>
     </body>
 </html>
-<style>
-body{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 80vh;
-}
-.container {
-    width: 80%;
-    max-width: 1000px;
-    height: 200px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-}
-button {
-    width: 400px;
-    height: 75px;
-    background-color: #FFFFFF;
-    border: 4px solid #2C80F3;
-}
-button:hover {
-    background-color: #2C80F3;
-    color: #FFFFFF;
-}
-</style>
-    
+
