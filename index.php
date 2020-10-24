47f7e2ebd3b2c65a3bbf0efdbda47c3048c65e4f
kspace-trk <keigonakao.0825@gmail.com>
2020-10-25 03:52:21 +0900

    first

--- index.php
+++ index.php
@@ -7,27 +7,30 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <div class="title">
-            <p>課題8-2</p>
-        </div>
-        <form action="addRecord.php" method="post" class="contents">
-        <div class="input-form">
-            <p>today</p>
-            <input type="text" name="today"></input>
-        </div>
-        <div class="input-form">
-            <p>inPrice</p>
-            <input type="text" name="inPrice"></input>
-        </div>
-        <div class="input-form">
-            <p>outPrice</p>
-            <input type="text" name="outPrice"></input>
-        </div>
-        <div class="input-form">
-            <p>contents</p>
-            <input type="text" name="contents"></input>
-        </div>
-            <button type="submit">レコード追加</button>
-        </form>
+    <?php
+        $dbname = 'testdb2';
+        $tablename = 'table1';
+
+        $link = mysqli_connect('127.0.0.1','root','dbpass',$dbname);
+        if(! $link){ exit("Connect error!"); }
+
+        $result = mysqli_query($link,"SELECT * FROM $tablename");
+        if(!$result){ exit("Select error on table ($tablename)!"); } 
+
+        while($row = mysqli_fetch_row($result))
+        {
+            echo "<hr>";
+            foreach($row as $key => $value)
+            {
+                echo htmlspecialchars($key) . "  : ";
+                echo htmlspecialchars($value) . "<br>";
+            }
+        }
+        echo "<hr>";
+
+        mysqli_free_result($result);
+
+        mysqli_close($link);
+    ?>
     </body>
 </html>
