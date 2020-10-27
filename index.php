956b173eae825b3c76279e803fe58c67b51839c7
kspace-trk <keigonakao.0825@gmail.com>
2020-10-27 22:18:43 +0900

    [add]動き

--- index.php
+++ index.php
@@ -5,8 +5,56 @@
         <title>なんちゃって掲示板システム</title>
     </head>
     <body>
+        <?php
+            if(isset($_POST['article'])){
+                $article=$_POST['article'];
+                echo $article;
+            }
+            $hostname = '127.0.0.1';
+            $username = 'root';
+            $password = 'dbpass';
+            $dbname = 'article_post';
+            $tablename = 'article_list';
+
+            $link = mysqli_connect($hostname,$username,$password);
+            if(!$link){
+                exit("Connect error!");
+            }
+            $result = mysqli_query($link,"USE $dbname");
+            //ここからないとき
+            if(!$result) {
+                $result = mysqli_query($link,"CREATE DATABASE $dbname CHARACTER SET utf8");
+                if(!$result) {
+                    echo "Create database $dbname failed!\n";
+                }
+                $result = mysqli_query($link,"USE $dbname");
+                if(!$result) {
+                        echo "USE failed!";
+                }
+                $result = mysqli_query($link,"CREATE TABLE $tablename (id INT NOT NULL AUTO_INCREMENT, article VARCHAR(840)BINARY, PRIMARY KEY(id)) CHARACTER SET utf8");
+                if(!$result) {
+                    exit("Create table $tablename failed!\n");
+                } 
+            }
+            //ここまでないとき
+
+            if(isset($_POST['article'])){
+                $result = mysqli_query($link,"INSERT INTO $tablename SET article='$article'");
+                if(! $result){
+                    exit("INSERT error!");
+                }
+                echo "Create db and table and update succeeded!\n";
+            }
+
+            mysqli_close($link);
+        ?>
         <div class="container">
         <p>なんちゃって掲示板システム</p>
+        1   <?php
+                if(isset($_POST['article'])){
+                    echo $article;
+                }
+            ?>
             <button onclick="location.href='articlePost.php'">新規投稿</button>
         </div>
     </body>
