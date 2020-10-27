7d2c5a5158895b50646b51daba9384e4378160de
kspace-trk <keigonakao.0825@gmail.com>
2020-10-27 21:24:06 +0900

    aa

--- index.php
+++ index.php
@@ -2,55 +2,41 @@
 <html>
     <head>
         <meta charset="UTF-8" />
-        <!-- vim: set sts sw=4 expandtab : -->
-        <title>ページタイトル</title>
-        <link rel="stylesheet" href="style.css">
+        <title>なんちゃって掲示板システム</title>
     </head>
     <body>
-    <?php
-        $dbname = 'testdb2';
-        $tablename = 'money_management_list';
-
-        $link = mysqli_connect('127.0.0.1','root','dbpass',$dbname);
-        if(! $link){ exit("Connect error!"); }
-
-        $result = mysqli_query($link,"SELECT * FROM $tablename");
-        if(!$result){ exit("Select error on table ($tablename)!"); } 
-        echo "<table>";
-            echo "<tr>";
-                echo "<td>";
-                    echo "id";
-                echo "</td>";
-                echo "<td>";
-                    echo "日付";
-                echo "</td>";
-                echo "<td>";
-                    echo "収入";
-                echo "</td>";
-                echo "<td>";
-                    echo "支出";
-                echo "</td>";
-                echo "<td>";
-                    echo "メモ";
-                echo "</td>";
-            echo "</tr>";
-            while($row = mysqli_fetch_row($result))
-            {
-                
-                echo "<tr>";
-                foreach($row as $key => $value)
-                {
-                    echo "<td>";
-                        echo htmlspecialchars($value);
-                    echo "</td>";
-                }
-            }
-            echo "</tr>";
-        echo "</table>";
-
-        mysqli_free_result($result);
-
-        mysqli_close($link);
-    ?>
+        <div class="container">
+        <p>なんちゃって掲示板システム</p>
+            <button onclick="location.href='articlePost.php'">新規投稿</button>
+        </div>
     </body>
 </html>
+<style>
+body{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 80vh;
+}
+.container {
+    width: 80%;
+    max-width: 1000px;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+}
+button {
+    width: 400px;
+    height: 75px;
+    background-color: #FFFFFF;
+    border: 4px solid #2C80F3;
+}
+button:hover {
+    background-color: #2C80F3;
+    color: #FFFFFF;
+}
+</style>
+    
